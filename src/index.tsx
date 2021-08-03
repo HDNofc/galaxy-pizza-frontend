d5efcbafe0cdaccf32a63a41cf3d7573d830c49a
vadim <holy.d.nexus@gmail.com>
2021-08-03 19:46:24 +0300

    Добавление переменных стилей

--- src/index.tsx
+++ src/index.tsx
@@ -5,12 +5,17 @@ import { Provider } from 'react-redux';
 import GlobalStyle from './styles/global';
 
 import store from './redux/store/store';
+import { ThemeProvider } from 'styled-components/macro';
+
+import variables from 'styles/variables';
 
 ReactDOM.render(
   // <React.StrictMode>
   <Provider store={store}>
+    <ThemeProvider theme={variables}>
       <GlobalStyle />
-    <App />
+      <App />
+    </ThemeProvider>
   </Provider>,
   // </React.StrictMode>,
   document.getElementById('root')
---
+++ src/styles/variables.tsx
@@ -0,0 +1,48 @@
+const common = {
+  tabletPortrait: '768px',
+  tabletLandscape: '1020px',
+  desktop: '1280px',
+};
+
+const fonts = {
+  fontMain: `'Open Sans', arial, helvetica, sans-serif`,
+  fontAdditional: `'Roboto', arial, helvetica, sans-serif`,
+};
+
+const colors = {
+  fontColorTitle: '#000',
+  fontColorText: '#5c6370',
+
+  buttonColorDefault: '#027ad6',
+  buttonColorTextDefault: '#fff',
+  buttonColorFocus: '#0262ad',
+  buttonColorActive: '#60a7de',
+  buttonColorTextActive: '#0e79cb',
+};
+
+const media = {
+  tabletPortrait: `(min-width: ${common.tabletPortrait})`,
+  tabletLandscape: `(min-width: ${common.tabletLandscape})`,
+  desktop: `(min-width: ${common.desktop})`,
+};
+
+const mixins = {
+  visuallyHidden: {
+    position: 'absolute',
+    width: '1px',
+    height: '1px',
+    margin: '-1px',
+    overflow: 'hidden',
+    clip: 'rect(0 0 0 0)',
+  },
+};
+
+const variables = {
+  common,
+  media,
+  mixins,
+  colors,
+  fonts,
+};
+
+export default variables;
