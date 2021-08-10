f1ec598d2327d54361c09b700d404b084f93c96f
vadim <holy.d.nexus@gmail.com>
2021-08-10 17:46:50 +0300

    Переименование App -> app

---
+++ src/app.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import MainApp from './views/main';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route component={MainApp} />
+      </Switch>
+    </BrowserRouter>
+  );
+}
+
+export default App;
