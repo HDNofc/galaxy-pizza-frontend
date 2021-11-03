a74b16defcf2482a5cfcdd64e5c6f6355ef19cb0
vadim <holy.d.nexus@gmail.com>
2021-11-03 12:03:50 +0300

    Включение StrictMode

--- src/index.tsx
+++ src/index.tsx
@@ -10,15 +10,15 @@ import GlobalStyle from './styles/global';
 import variables from 'styles/variables';
 
 ReactDOM.render(
-  // <React.StrictMode>
-  <Provider store={store}>
-    <StyleSheetManager disableVendorPrefixes={process.env.NODE_ENV === 'development'}>
-      <ThemeProvider theme={variables}>
-        <GlobalStyle />
-        <App />
-      </ThemeProvider>
-    </StyleSheetManager>
-  </Provider>,
-  // </React.StrictMode>,
+  <React.StrictMode>
+    <Provider store={store}>
+      <StyleSheetManager disableVendorPrefixes={process.env.NODE_ENV === 'development'}>
+        <ThemeProvider theme={variables}>
+          <GlobalStyle />
+          <App />
+        </ThemeProvider>
+      </StyleSheetManager>
+    </Provider>
+  </React.StrictMode>,
   document.getElementById('root')
 );
